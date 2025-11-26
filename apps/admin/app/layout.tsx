a80bccc0bb2e49b14d0b5e56c92fea595d4be98a
Jeremiah <sentomerojeremy@gmail.com>
2025-11-26 23:49:45 +0300

    updating the favicons

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="vendly" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
