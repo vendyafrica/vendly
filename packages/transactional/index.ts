7f3d69be0958e4596b5bbfbec6013277fe0c2dc1
Jeremiah <sentomerojeremy@gmail.com>
2025-12-03 23:16:29 +0300

    done with email verification

--- packages/transactional/index.ts
+++ packages/transactional/index.ts
@@ -1,6 +1,7 @@
 import { render } from '@react-email/components';
-import { VerificationEmail } from './emails/verification';
+import { VerificationEmail } from './emails/verification.jsx';
 import * as React from 'react';
+
 export { VerificationEmail };
 
 export const renderVerificationEmail = async (props: { name: string; url: string }) => {
