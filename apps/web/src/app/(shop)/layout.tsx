26d28e8118770f27ce5a5348023ac2b9912e308f
Jeremiah <sentomerojeremy@gmail.com>
2025-12-23 00:21:14 +0300

    fixing node version and shop issues

--- apps/web/src/app/(shop)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function ShopLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- apps/web/src/app/(shop)/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ShopRootPage() {
-  return <div>Shop</div>;
-}
