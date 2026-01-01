3afbd30c497b09cf377e18ec82d1f63c8e426fe1
Jeremiah <sentomerojeremy@gmail.com>
2026-01-01 17:29:42 +0300

    setting up

--- apps/admin/src/app/(dashboard)/layout.tsx
+++ apps/admin/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { Separator } from "@vendly/ui/components/separator"
 import {
   SidebarInset,
   SidebarProvider,
@@ -23,10 +22,6 @@ export default function DashboardLayout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
